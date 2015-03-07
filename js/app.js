75f86dd44d1296c0cb7fc3aa843b86cd76047ca7
David Mandel <MandelDL@gmail.com>
2015-03-07 14:15:23 -0500

    add routes for outbox and outbox/id to connect to controllers and views

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 var app = angular.module('OutboxApp', ['ngRoute']);
 app.config(function ($routeProvider) { 
   $routeProvider 
-    .when('/', { 
+    .when('/outbox', { 
       controller: 'HomeController', 
       templateUrl: 'views/home.html' 
-    }) 
+    })
+    .when('/outbox/:id', {
+    	controller: 'EmailController',
+    	templateUrl: 'views/email.html'
+    })
     .otherwise({ 
       redirectTo: '/' 
     }); 
